b0fc5faa06b1619d42001c7a692008649d281a4d
ch405maki <markmanuel0317@gmail.com>
2025-07-24 15:56:01 +0800

    working on pagination

--- resources/js/pages/Logs/Index.vue
+++ resources/js/pages/Logs/Index.vue
@@ -228,9 +228,8 @@ const clearDateFilter = () => {
             </div>
 
             <!-- Pagination -->
-            <!-- Updated Pagination Section -->
-            <Pagination v-if="logs && logs.links.length > 3">
-                <PaginationContent class="space-x-1">
+            <Pagination >
+                <PaginationContent class="space-x-4">
                     <template v-for="(link, index) in logs.links" :key="index">
                     <PaginationItem v-if="link.url">
                         <a
@@ -249,27 +248,7 @@ const clearDateFilter = () => {
                     </PaginationItem>
                     </template>
                 </PaginationContent>
-                </Pagination>
-
-                <Pagination v-slot="{ page }" :items-per-page="10" :total="30" :default-page="2">
-                    <PaginationContent v-slot="{ items }">
-                    <PaginationPrevious />
-
-                    <template v-for="(item, index) in items" :key="index">
-                        <PaginationItem
-                        v-if="item.type === 'page'"
-                        :value="item.value"
-                        :is-active="item.value === page"
-                        >
-                        {{ item.value }}
-                        </PaginationItem>
-                    </template>
-
-                    <PaginationEllipsis :index="4" />
-
-                    <PaginationNext />
-                    </PaginationContent>
-                </Pagination>
+            </Pagination>
         </div>
     </AppLayout>
 
