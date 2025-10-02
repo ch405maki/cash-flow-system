bc614f22089c5a487e9ef2682c431d1f2938ebe5
ch405maki <markmanuel0317@gmail.com>
2025-10-02 16:00:58 +0800

    working on form layout

--- resources/js/components/pettyCash/printables/PettyCash.vue
+++ resources/js/components/pettyCash/printables/PettyCash.vue
@@ -22,51 +22,79 @@ const props = defineProps<{
       </div>
     </div>
 
-    <!-- Checkboxes -->
-    <div class="grid grid-cols-3 gap-2 mt-2 border border-black">
-      <label class="flex items-center border-r border-black p-1">
-        <input type="checkbox" disabled :checked="props.pettyCash.type === 'Reimbursement'" class="mr-1" />
-        <span>PAYMENT REIMBURSEMENT</span>
-      </label>
-      <label class="flex items-center border-r border-black p-1">
-        <input type="checkbox" disabled :checked="props.pettyCash.type === 'Cash Advance'" class="mr-1" />
-        <span>CASH ADVANCE (CA)</span>
-      </label>
-      <label class="flex items-center p-1">
-        <input type="checkbox" disabled :checked="props.pettyCash.type === 'Liquidation'" class="mr-1" />
-        <span>LIQUIDATION OF CA DATED ______</span>
-      </label>
+    <div class="grid grid-cols-5 border-x border-t border-black">
+        <!-- Checkboxes -->
+        <div class="col-span-3">
+            <div class="grid grid-cols-3 gap-2 ">
+                <label class="flex items-center space-x-1 border-r border-black p-1">
+                <input type="checkbox" disabled :checked="props.pettyCash.items.some(i => i.type === 'Reimbursement')" />
+                    <span>PAYMENT REIMBURSEMENT</span>
+                </label>
+                <label class="flex items-center space-x-1 border-r border-black p-1">
+                    <input type="checkbox" disabled :checked="props.pettyCash.items.some(i => i.type === 'Cash Advance')" />
+                    <span>CASH ADVANCE (CA)</span>
+                </label>
+                <label class="flex items-center space-x-1 border-r border-black p-1">
+                    <input type="checkbox" disabled :checked="props.pettyCash.items.some(i => i.type === 'Liquidation')" />
+                    <span>LIQUIDATION OF CA DATED ________</span>
+                </label>
+            </div>
+        </div>
+        <div class="col-span-2 ">
+            <div class="grid grid-cols-2 gap-2">
+                <label class="flex items-center space-x-1 border-r border-black p-1">
+                    ACCOUNT NAME
+                </label>
+                <label class="flex items-center space-x-1 p-1">
+                    AMOUNT
+                </label>
+            </div>
+        </div>
     </div>
 
     <!-- Table -->
-    <table class="w-full border border-black border-collapse mt-2">
-      <thead>
-        <tr>
-          <th class="border border-black w-[80px] p-1">DATE</th>
-          <th class="border border-black p-1">PARTICULARS</th>
-          <th class="border border-black w-[100px] p-1">AMOUNT</th>
-          <th class="border border-black w-[200px] p-1">ACCOUNT NAME</th>
-          <th class="border border-black w-[100px] p-1">AMOUNT</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="item in props.pettyCash.items" :key="item.id">
-          <td class="border border-black text-center p-1">{{ new Date(item.date).toLocaleDateString() }}</td>
-          <td class="border border-black p-1">{{ item.particulars }}</td>
-          <td class="border border-black text-right p-1">{{ Number(item.amount).toFixed(2) }}</td>
-          <td class="border border-black p-1"></td>
-          <td class="border border-black p-1"></td>
-        </tr>
-        <!-- Filler rows -->
-        <tr v-for="n in 6" :key="'empty-'+n">
-          <td class="border border-black p-4"></td>
-          <td class="border border-black"></td>
-          <td class="border border-black"></td>
-          <td class="border border-black"></td>
-          <td class="border border-black"></td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="grid grid-cols-5">
+        <div class="col-span-3">
+            <table class="w-full border border-black border-collapse">
+                <thead>
+                    <tr>
+                    <th class="border border-black w-[80px] p-1">DATE</th>
+                    <th class="border border-black p-1">PARTICULARS</th>
+                    <th class="border border-black w-[100px] p-1">AMOUNT</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="item in props.pettyCash.items" :key="item.id">
+                    <td class="border border-black text-center p-1">{{ new Date(item.date).toLocaleDateString() }}</td>
+                    <td class="border border-black p-1">{{ item.particulars }}</td>
+                    <td class="border border-black text-right p-1">{{ Number(item.amount).toFixed(2) }}</td>
+                    </tr>
+                    <!-- Filler rows -->
+                    <tr v-for="n in 6" :key="'empty-'+n">
+                    <td class="border border-black p-3"></td>
+                    <td class="border border-black"></td>
+                    <td class="border border-black"></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="col-span-2">
+            <table class="w-full border-r border-black border-collapse">
+                <tbody>
+                    <tr v-for="item in props.pettyCash.items" :key="item.id">
+                    <td class="border-t border-black text-center p-3"></td>
+                    <td class="border border-black p-1"></td>
+                    </tr>
+                    <!-- Filler rows -->
+                    <tr v-for="n in 7" :key="'empty-'+n">
+                    <td class="border-y border-black p-3"></td>
+                    <td class="border border-black"></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    
 
     <!-- Received from -->
     <div class="mt-2">
