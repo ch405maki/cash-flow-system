2ee954a4876c39fe799987429ed7571655b4a33c
ch405maki <markmanuel0317@gmail.com>
2025-10-04 09:55:23 +0800

    working on printables

--- resources/js/components/pettyCash/printables/PettyCash.vue
+++ resources/js/components/pettyCash/printables/PettyCash.vue
@@ -10,15 +10,37 @@ const props = defineProps<{
     <div class="text-center">
       <h2 class="font-bold uppercase">ARELLANO LAW FOUNDATION INC.</h2>
       <p>Taft Avenue Corner Menlo Street, Pasay City</p>
-      <h3 class="font-bold uppercase underline mt-1">PETTY CASH VOUCHER</h3>
+      
     </div>
 
     <!-- Paid to + Date / PCV No -->
-    <div class="flex justify-between items-end mt-2">
-      <p>Paid to : <span class="font-semibold underline">{{ props.pettyCash.paid_to }}</span></p>
-      <div class="text-right">
-        <p>Date: <span class="underline">{{ new Date(props.pettyCash.date).toLocaleDateString() }}</span></p>
-        <p>PCV No.: <span class="underline">{{ props.pettyCash.pcv_no }}</span></p>
+    <div class="grid grid-cols-5 border-x border-t border-black">
+      <div class="col-span-3 border-r border-black">
+          <div class="grid grid-cols-3 gap-2 ">
+              <h1 class="font-bold uppercase mt-1">PETTY CASH VOUCHER</h1>
+          </div>
+      </div>
+      <div class="col-span-2">
+          <div class="grid grid-cols-2 gap-2">
+            <p class="pr-2 border-r border-black">
+              Date: <span>{{ new Date(props.pettyCash.date).toLocaleDateString() }}</span>
+            </p>
+            <p class="pl-2">
+              PCV No.: <span>{{ props.pettyCash.pcv_no }}</span>
+            </p>
+          </div>
+      </div>
+    </div>
+    <div class="grid grid-cols-5 border-x border-t border-black">
+      <div class="col-span-3 border-r border-black">
+          <div class="grid grid-cols-3 gap-2 ">
+              <h1>Paid to : <span class="font-semibold underline">{{ props.pettyCash.paid_to }}</span></h1>
+          </div>
+      </div>
+      <div class="col-span-2 ">
+          <div class="grid grid-cols-2 gap-2">
+              <h1>Distribution of Expense</h1>
+          </div>
       </div>
     </div>
 
@@ -77,53 +99,61 @@ const props = defineProps<{
                     </tr>
                 </tbody>
             </table>
+            <div class="grid grid-cols-1 grid-rows-3 gap-0 border-x border-black">
+              <div class="border-b border-black">
+                <!-- Received from -->
+                <div class="mt-2">
+                  <p>Received from the amount of ______________________________</p>
+                  <p>(P __________ ) in full/partial payment of the items listed above.</p>
+                </div>
+              </div>
+              <div class="border-b border-black">
+                <!-- Notes -->
+                <div class="mt-2 text-[11px] leading-tight">
+                  <p><b>NOTE:</b> CASH ADVANCE SHALL BE LIQUIDATED WITHIN 72 HOURS; OTHERWISE, I HEREBY AUTHORIZE THE COMPANY TO DEDUCT THE FULL AMOUNT FROM MY NEAREST SUCCEEDING PAYROLL.</p>
+                </div>
+              </div>
+              <div class="border-b border-black">
+                <!-- Bottom -->
+                <div class="flex justify-between">
+                  <p>Signature: ____________________</p>
+                  <p>Date: ____________________</p>
+                </div>
+              </div>
+          </div>
         </div>
+        
         <div class="col-span-2">
-            <table class="w-full border-r border-black border-collapse">
-                <tbody>
-                    <tr v-for="item in props.pettyCash.items" :key="item.id">
-                    <td class="border-t border-black text-center p-3"></td>
-                    <td class="border border-black p-1"></td>
-                    </tr>
-                    <!-- Filler rows -->
-                    <tr v-for="n in 7" :key="'empty-'+n">
-                    <td class="border-y border-black p-3"></td>
-                    <td class="border border-black"></td>
-                    </tr>
-                </tbody>
-            </table>
+          <table class="w-full table-fixed border-collapse">
+            <tbody>
+              <tr v-for="item in props.pettyCash.items" :key="item.id">
+                <td class="w-1/2 border-t border-black text-center p-3"></td>
+                <td class="w-1/2 border border-black p-1"></td>
+              </tr>
+              <!-- Filler rows -->
+              <tr v-for="n in 5" :key="'empty-'+n">
+                <td class="w-1/2 border-y border-black p-3"></td>
+                <td class="w-1/2 border border-black"></td>
+              </tr>
+            </tbody>
+          </table>
+          
+          <div class="grid grid-cols-1 grid-rows-4 gap-0 border-r border-black">
+              <div class="border-b border-black">
+                <p>Prepared by: ____________________</p>
+              </div>
+              <div class="border-b border-black">
+                  <p>Approved by: ____________________</p>
+                  <p class="text-[11px]">(Signature over printed name)</p>
+              </div>
+              <div class="border-b border-black">
+                <p>Audited by: ____________________</p>
+              </div>
+              <div class="border-b border-black">
+                <p>Paid by: ____________________</p>
+              </div>
+          </div>
         </div>
-    </div>
-    
-
-    <!-- Received from -->
-    <div class="mt-2">
-      <p>Received from the amount of ______________________________</p>
-      <p>(P __________ ) in full/partial payment of the items listed above.</p>
-    </div>
-
-    <!-- Notes -->
-    <div class="mt-2 text-[11px] leading-tight">
-      <p><b>NOTE:</b> CASH ADVANCE SHALL BE LIQUIDATED WITHIN 72 HOURS; OTHERWISE, I HEREBY AUTHORIZE THE COMPANY TO DEDUCT THE FULL AMOUNT FROM MY NEAREST SUCCEEDING PAYROLL.</p>
-    </div>
-
-    <!-- Signatures -->
-    <div class="grid grid-cols-2 gap-6 mt-4">
-      <div>
-        <p>Prepared by: ____________________</p>
-        <p>Approved by: ____________________</p>
-        <p class="text-[11px]">(Signature over printed name)</p>
-      </div>
-      <div>
-        <p>Audited by: ____________________</p>
-        <p>Paid by: ____________________</p>
-      </div>
-    </div>
-
-    <!-- Bottom -->
-    <div class="flex justify-between mt-6">
-      <p>Signature: ____________________</p>
-      <p>Date: ____________________</p>
     </div>
   </div>
 </template>
