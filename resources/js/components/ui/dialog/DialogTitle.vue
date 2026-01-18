882d959ea03b3499fecd50d8d5f381a292a72cea
ch405maki <markmanuel0317@gmail.com>
2026-01-18 15:39:09 +0800

    rollback dialog folder

--- resources/js/components/ui/dialog/DialogTitle.vue
+++ resources/js/components/ui/dialog/DialogTitle.vue
@@ -1,27 +1,21 @@
 <script setup lang="ts">
-import type { DialogTitleProps } from "reka-ui"
-import type { HTMLAttributes } from "vue"
-import { reactiveOmit } from "@vueuse/core"
-import { DialogTitle, useForwardProps } from "reka-ui"
-import { cn } from "@/lib/utils"
+import { cn } from '@/lib/utils';
+import { DialogTitle, useForwardProps, type DialogTitleProps } from 'radix-vue';
+import { computed, type HTMLAttributes } from 'vue';
 
-const props = defineProps<DialogTitleProps & { class?: HTMLAttributes["class"] }>()
+const props = defineProps<DialogTitleProps & { class?: HTMLAttributes['class'] }>();
 
-const delegatedProps = reactiveOmit(props, "class")
+const delegatedProps = computed(() => {
+    const { class: _, ...delegated } = props;
 
-const forwardedProps = useForwardProps(delegatedProps)
+    return delegated;
+});
+
+const forwardedProps = useForwardProps(delegatedProps);
 </script>
 
 <template>
-  <DialogTitle
-    v-bind="forwardedProps"
-    :class="
-      cn(
-        'text-lg font-semibold leading-none tracking-tight',
-        props.class,
-      )
-    "
-  >
-    <slot />
-  </DialogTitle>
+    <DialogTitle v-bind="forwardedProps" :class="cn('text-lg font-semibold leading-none tracking-tight', props.class)">
+        <slot />
+    </DialogTitle>
 </template>
