e46a02d285f9500d3f67772aabf80712d8c54561
ch405maki <markmanuel0317@gmail.com>
2026-02-18 14:53:02 +0800

    udjusted print

--- resources/js/components/pettyCash/PettyCashEditForm.vue
+++ resources/js/components/pettyCash/PettyCashEditForm.vue
@@ -5,6 +5,7 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { usePage, router } from '@inertiajs/vue3'
 import { Textarea } from '@/components/ui/textarea'
+import { route } from 'ziggy-js'
 import {
   Table,
   TableBody,
