e967333df3c7fc879b3a47ddac9302afe643035b
ch405maki <markmanuel0317@gmail.com>
2026-06-24 14:57:44 +0800

    testing

--- resources/js/pages/auth/Login.vue
+++ resources/js/pages/auth/Login.vue
@@ -38,7 +38,7 @@ const submit = () => {
         <form @submit.prevent="submit" class="flex flex-col gap-6">
             <div class="grid gap-6">
                 <div class="grid gap-2">
-                    <Label for="login">Username</Label>
+                    <Label for="login">Username Testing</Label>
                     <Input
                         id="login"
                         type="text"
